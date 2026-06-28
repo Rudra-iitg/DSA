dd436c07e6c9a4ce1ae55a44c25da72b468d2ab3
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 01:34:23 +0530

    refactor: modify star pattern generation logic by commenting out spacing and updating star counts

--- 1.cpp
+++ 1.cpp
@@ -5,10 +5,10 @@ class star {
 public:
   void starpatten_up(int n) {
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n - i - 1; j++) {
-        cout << " ";
-      }
-      for (int j = 0; j < 2 * i + 1; j++) {
+      // for (int j = 0; j < n - i - 1; j++) {
+      // cout << " ";
+      //}
+      for (int j = 0; j < i + 1; j++) {
         cout << "*";
       }
       cout << endl;
@@ -18,11 +18,11 @@ public:
   void starpatten_down(int n) {
     for (int i = 0; i < n; i++) {
       // Print spaces
-      for (int j = 0; j < i + 1; j++) {
-        cout << " ";
-      }
+      // for (int j = 0; j < i + 1; j++) {
+      // cout << " ";
+      //}
       // Print stars
-      for (int j = 0; j < 2 * (n - i) - 1; j++) {
+      for (int j = 0; j < n - i; j++) {
         cout << "*";
       }
       cout << endl;
