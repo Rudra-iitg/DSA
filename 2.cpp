07e7ba30b77c3299cc2913947c74beb4d3911a7c
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 03:09:57 +0530

    refactor: update character pattern logic to iterate rows incrementally and print updated character values

--- 2.cpp
+++ 2.cpp
@@ -4,8 +4,9 @@ using namespace std;
 class pattern {
 public:
   void printpattern(int n) {
-    for (int i = n; i >= 1; i--) {
-      for (char ch = 'A'; ch <= 'A' + i - 1; ch++) {
+    for (int i = 1; i <= n; i++) {
+      char ch = 'A' + i - 1;
+      for (int j = 1; j <= i; j++) {
         cout << ch << " ";
       }
       cout << endl;
