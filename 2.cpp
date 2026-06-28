8377e48cfc28d7d80ee70ad8739fcc6e664f6ab3
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 02:51:36 +0530

    This the new files

--- 2.cpp
+++ 2.cpp
@@ -4,22 +4,13 @@ using namespace std;
 class pattern {
 public:
   void printpattern(int n) {
-    int space = 2 * n - 2;
+    int num = 1;
     for (int i = 1; i <= n; i++) {
-      // num
       for (int j = 1; j <= i; j++) {
-        cout << j;
-      }
-      // space
-      for (int j = 1; j <= space; j++) {
-        cout << " ";
-      }
-      // rev num
-      for (int j = i; j >= 1; j--) {
-        cout << j;
+        cout << num << " ";
+        num++;
       }
       cout << endl;
-      space = space - 2;
     }
   }
 };
