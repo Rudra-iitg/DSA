c68b163a5b274fcf18162efcf167d93c8bd800b7
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 15:18:51 +0530

    refactor: update character loop logic in pattern printing function

--- 3.cpp
+++ 3.cpp
@@ -5,7 +5,7 @@ class pattern {
 public:
   void printpattern(int n) {
     for (int i = 0; i <= n; i++) {
-      for (char j = 'A' + n; j >= 'A' + i; j--) {
+      for (char j = 'A' + n - i - 1; j <= 'A' + n - 1; j++) {
         cout << j << " ";
       }
       cout << endl;
