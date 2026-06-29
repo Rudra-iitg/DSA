1ac68a5ed7db72e04f5f807d5589f34a024cf0e3
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 23:43:33 +0530

    feat: add C++ implementation for calculating greatest common divisor

---
+++ gcd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int num1, int num2) {
+  for (int i = min(num1, num2); i > 0; i--) {
+    if (num1 % i == 0 && num2 % i == 0) {
+      return i;
+    }
+  }
+}
+
+int main() {
+  int num1 = 4425;
+  int num2 = 545;
+  cout << "The GCD of " << num1 << " and " << num2 << " is: " << gcd(num1, num2)
+       << endl;
+  return 0;
+}
