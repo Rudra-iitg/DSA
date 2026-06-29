1e18eba702e4446b9b3769c241bed67501b03865
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 15:46:45 +0530

    refactor: implement logarithmic digit counting method and comment out legacy brute force logic

--- 4.cpp
+++ 4.cpp
@@ -1,22 +1,33 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+
 using namespace std;
+
+// OPtimal method
+
+int countdigit(int n) {
+  int count = (int)(log10(n) + 1);
+  return count;
+}
 int main() {
-  auto num = 34454545326;
-  int count = 0;
-  vector<int> new_vec;
-  while (num > 0) {
-    int x = num % 10;
-    new_vec.push_back(x);
-    num /= 10;
-    count++;
-  }
-  reverse(new_vec.begin(), new_vec.end());
-  for (int i = 0; i < new_vec.size(); i++) {
-    cout << new_vec[i] << " ";
-  }
-  cout << '\n';
-  cout << "The number of digit is " << count;
-  cout << endl;
+  // *******************. BRUTE FORCE METHOD ***********************
+  auto num = 3445454;
+  //   int count = 0;
+  //   vector<int> new_vec;
+  //   while (num > 0) {
+  //     int x = num % 10;
+  //     new_vec.push_back(x);
+  //     num /= 10;
+  //     count++;
+  //   }
+  //   reverse(new_vec.begin(), new_vec.end());
+  //   for (int i = 0; i < new_vec.size(); i++) {
+  //     cout << new_vec[i] << " ";
+  //   }
+  //   cout << '\n';
+  cout << "The number of digit is " << countdigit(num);
+  //   cout << endl;
 
   return 0;
 }
