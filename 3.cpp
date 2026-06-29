21da75d4e1082cd3cfc4e88c2d2e335f15ae3460
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-06-29 12:22:31 +0530

    refactor: update printpattern logic to display descending character sequences

--- 3.cpp
+++ 3.cpp
@@ -5,14 +5,8 @@ class pattern {
 public:
   void printpattern(int n) {
     for (int i = 0; i <= n; i++) {
-      for (int j = n; j > i; j--) {
-        cout << " ";
-      }
-      for (char k = 'A'; k < 'A' + i; k++) {
-        cout << k;
-      }
-      for (char l = 'A' + i - 2; l >= 'A'; l--) {
-        cout << l;
+      for (char j = 'A' + n; j >= 'A' + i; j--) {
+        cout << j << " ";
       }
       cout << endl;
     }
