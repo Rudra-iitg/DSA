757b42dcd920afcc49ce0be71229a89e1b89ccc3
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-07-05 01:00:33 +0530

    Add solution for removing duplicates from a sorted array
    - Implemented a class `Solution` with a method `removeDuplicate` that removes duplicates from a sorted vector of integers.
    - The method modifies the input vector in place and returns the count of unique elements.
    - Added a main function to demonstrate the functionality with a sample input.

---
+++ 6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+    public:
+    int removeDuplicate(vector<int> &num){
+        if(num.empty()) return 0;
+        
+        int i = 0;
+        for(int j = 1; j < num.size(); j++){
+            if(num[j] != num[i]){
+                i++;
+                num[i] = num[j];
+            }
+        }
+        return i + 1;
+    }
+};
+
+int main(){
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};
+    Solution sol;
+    int k = sol.removeDuplicate(nums);
+    cout << "Unique coutn : " << k << '\n';
+    cout << "Array after removing duplicate: " ;
+    for(int i = 0; i < k ; i++){
+        cout << nums[i] << "  " << '\n';
+    }
+    cout << endl;
+    return 0;
+}
