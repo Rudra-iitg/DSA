362dfd4e50967056996c13243aeccfd846914c93
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-07-19 20:53:47 +0530

    Refactor student class to implement parameterized constructor and update main function for object initialization

--- 1.cpp
+++ 1.cpp
@@ -9,8 +9,16 @@ class student{
     int num;
 
     //constructor
-    student(){
+    // student(){
+    //     cout << "Student deflault constructor called\n";
+    // }
+    //PARAMATERISED CONSTRUCTOR
+    student(int id, int age, string name, int num){
         cout << "Student deflault constructor called\n";
+        this -> id = id;
+        this -> age = age;
+        this -> name = name;
+        this -> num = num;
     }
     void study(){
         cout << this -> name << " Studying\n ";
@@ -27,15 +35,22 @@ class student{
 
 int main(){
     cout << "=========================\n";
-    student A;
-    A.age = 15;
-    A.id = 001;
-    A.name = "Dhanush"; 
-    A.study();
-    student B;
-    B.age = 135;
-    B.id = 002;
-    B.name = "Anand"; 
+    // student A;
+    // A.age = 15;
+    // A.id = 001;
+    // A.name = "Dhanush"; 
+    // A.study();
+    // student B;
+    // B.age = 135;
+    // B.id = 002;
+    // B.name = "Anand"; 
+    // A.sleep();
+    student A(1, 5, "Keshab", 4);
+    student B(4, 6, "Rishab", 4);
+    student C(9, 11, "Sahab", 4);
+    student D(7, 11, "Rakshab", 4);
+    cout << A.name << " " << A.age << endl;
     A.sleep();
+    B.study();
     return 0;
 }
