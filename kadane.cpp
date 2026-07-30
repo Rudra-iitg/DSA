504bd5ce9d0d5cbe286c8125bd108ffaab0457de
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-07-31 01:14:39 +0530

    Refactor max_sum function to implement Kadane's algorithm for maximum subarray sum

--- kadane.cpp
+++ kadane.cpp
@@ -5,14 +5,22 @@ using namespace std;
 class Solution{
     public:
         int max_sum(vector<int> arr, int n){
-            long long max_sum = LLONG_MIN;
-            long long new_sum = 0;
-            for(int i = 0; i < n ; i++){
-                new_sum += arr[i];
-                max_sum = max(max_sum , new_sum);
-                if(new_sum < 0){
-                    new_sum = 0;
-                }
+            // OPTIMAL APPROACH
+            // long long max_sum = LLONG_MIN;
+            // long long new_sum = 0;
+            // for(int i = 0; i < n ; i++){
+            //     new_sum += arr[i];
+            //     max_sum = max(max_sum , new_sum);
+            //     if(new_sum < 0){
+            //         new_sum = 0;
+            //     }
+            // }
+            // OPTIMAL APPRACH
+            //================MOST OPTIMAL APPRACH ===============
+            int max_sum = arr[0], current_sum = arr[0];
+            for(int i = 1; i < n ; i++){
+                current_sum = max(arr[i], current_sum + arr[i]);
+                max_sum = max(current_sum , max_sum);
             }
             return max_sum;
         }
