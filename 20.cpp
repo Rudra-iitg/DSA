ed82913d249d15f15d0eee64a70aec952e108369
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-08-03 15:06:26 +0530

    Add solution for finding the longest consecutive elements in an array
    - Implemented the `longest_arr` function in the `Solution` class to calculate the length of the longest consecutive elements sequence in a given vector of integers.
    - Utilized an unordered set to store unique elements and efficiently check for consecutive numbers.
    - Added a main function to demonstrate the functionality with a sample input vector.

---
+++ 20.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+class Solution{
+    public:
+        int longest_arr(vector<int> nums){
+            int n = nums.size();
+            if(n == 0){
+                return 0;
+            }
+            int longest = 1;
+            unordered_set<int> st;
+            for(int i = 0; i < n ; i++){
+                st.insert(nums[i]);
+            }
+            for(auto it : st){
+                if(st.find(n-1) == st.end()){
+                    int cnt = 1;
+                    int x = it;
+
+                    while(st.find(x + 1) != st.end()){
+                        x += 1;
+                        cnt += 1;
+                    }
+                    longest = max(cnt, longest);
+                }
+            }
+            return longest;
+        }
+};
+int main(){
+    vector<int> arr = {100, 4, 200, 1, 3, 2};
+    cout << "Given Vactor : {  ";
+    for(int x : arr){
+        cout << x << "  " ;
+    }
+
+    cout << "}";
+    cout << endl;
+    Solution Sol;
+    cout << "The longest Consecutrve elemtens are: "<< Sol.longest_arr(arr)<< endl;
+    return 0;
+}
