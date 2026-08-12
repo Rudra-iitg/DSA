ce5c3a0dcdb1444a0867b58b10491a04ac7cd719
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-08-13 00:00:03 +0530

    Refactor cp.cpp to implement a new game logic for Anton and Danik

--- cp.cpp
+++ cp.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+// #include <cctype>
+// using namespace std;
+// int main(){
+//     string str1;
+//     string str2;
+//     cin >> str1 >> str2;
+//     for (char &c : str1){
+//         c = toupper(c);
+//     }
+//     for (char &c : str2){
+//         c = toupper(c);
+//     }
+//     for(int i = 0 ; i < str1.size(); i++){
+//         if(str1[i] == str2[i]){
+//             if(i == str2.size() - 1){
+//                 cout << 0;
+//                 break;
+//             }
+//             else{
+//                 continue;
+//             }    
+//         }
+//         else if(str1[i] < str2[i]){
+//             cout << -1;
+//             break;
+//         }
+//         else{
+//             cout << 1;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// +++++++++++++734A==================
+#include <iostream>
+#include <string>
 using namespace std;
 int main(){
-    string str1;
-    string str2;
-    cin >> str1 >> str2;
-    for (char &c : str1){
-        c = toupper(c);
+    int n;
+    string st;
+    cin >> n;
+    cin >> st;
+    int a = 0, d = 0;
+    for(int i = 0 ; i < n; i++){
+        if(st[i] == 'A') a++;
+        else d++;
     }
-    for (char &c : str2){
-        c = toupper(c);
+    if(a > d){
+        cout << "Anton";
     }
-    for(int i = 0 ; i < str1.size(); i++){
-        if(str1[i] == str2[i]){
-            if(i == str2.size() - 1){
-                cout << 0;
-                break;
-            }
-            else{
-                continue;
-            }    
-        }
-        else if(str1[i] < str2[i]){
-            cout << -1;
-            break;
-        }
-        else{
-            cout << 1;
-            break;
-        }
+    else if(d > a){
+        cout << "Danik";
+    }
+    else {
+        cout << "Friendship";
     }
     return 0;
 }
