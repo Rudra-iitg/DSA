c9f29964a053d6e791fbdd20d0531b62872640e6
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-08-18 01:37:17 +0530

    Enhance iterator1.cpp with additional iterator examples and logic for filtering and modifying elements

--- iterator1.cpp
+++ iterator1.cpp
@@ -1,13 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <iterator> // Needed for task 4;
 using namespace std;
 int main(){
-    vector<int> v = {4,64,43,7,23,64,7};
-    for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
-        if(*it > 10){
+    {
+        vector<int> v = {4,64,43,7,23,64,7};
+        for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
+            if(*it > 10){
+                cout << *it << "  ";
+            }
+        }
+        cout << endl;
+    }
+    {
+        vector<int> v = {3,5,7,13,65, 54, 33};
+        for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
+            *it *= 2;
             cout << *it << "  ";
         }
+        cout << endl;
     }
-    cout << endl;
+    {
+        vector<string> word = {"C++", "is" , "fun"};
+        for(vector<string>::reverse_iterator it = word.rbegin(); it != word.rend(); ++it){
+            cout << *it << "  ";
+        } 
+        cout << endl;
+    }
+    {   //This it += 2 or any num works only on vector or array, 
+        vector<int> v = {4, 64,2,5,52,67,36,9,39,76,24,76,23,71,53,75};
+        for(vector<int> ::iterator it = v.begin(); it != v.end(); it += 2){
+            cout << *it << "  ";
+        }
+        cout << endl;
+    }
+    {
+        vector<int> v = {4, 64,2,5,52,67,36,9,39,76,24,76,23,71,53,75};
+        vector<int> ::iterator it = v.begin();
+        advance(it, 2);
+        cout << "Element : " << *it << endl;
+        cout << *it << "  ";
+        int steps = distance(it, v.end());
+        cout << steps ;
+        cout << endl; 
+    }
+    {
+        vector<int> v = {1, 2, 3, 4, 5, 6};
+        for(auto it = v.begin(); it != v.end(); ){
+            if(*it % 2 == 0){
+                it = v.erase(it);
+            }
+            else{
+                ++it;
+            }
+        }
+        for(int x : v){
+            cout << x << "  ";
+        }
+        cout << endl;
+    }
     return 0;
 }
+
