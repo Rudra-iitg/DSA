b3b41f007a3cbb980964593464f8e6fb774852c7
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-08-20 00:14:43 +0530

    Refactor cp_344A.cpp to implement new logic for array indexing and add tempCodeRunnerFile.cpp with initial group counting code

--- cp_344A.cpp
+++ cp_344A.cpp
@@ -1,16 +1,67 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int group = 0;
+//     int first_mag ;
+//     int second_mag = 0;
+//     for(int i = 0 ; i < n ; i++){
+//         cin >> first_mag;
+//         if(first_mag != second_mag) group++;
+//         second_mag = first_mag;
+//     }
+//     cout << group;
+//     return 0;
+// }
+
+//=================. 200B ===================
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     double sum = 0;
+//     for(int  i = 0; i < n ; i++){
+//         int a;
+//         cin >> a;
+//         sum += a;
+//     }
+//     cout << sum /n;
+//     return 0;
+// }
+
+//=============== 228A=================
+
+// #include <iostream>
+// #include <set>
+// using namespace std;
+// int main(){
+//     set<int> color;
+//     for(int i = 0 ; i < 4; i++){
+//         int a;
+//         cin >> a;
+//         color.insert(a);
+//     }
+//     cout << 4 - color.size();
+//     return 0;
+// }
+//=================136A=================
+
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
     int n;
     cin >> n;
-    int group = 0;
-    int first_mag ;
-    int second_mag = 0;
-    for(int i = 0 ; i < n ; i++){
-        cin >> first_mag;
-        if(first_mag != second_mag) group++;
-        second_mag = first_mag;
+    vector<int> arr(n);
+    for(int i = 0; i < n ; i++){
+        int a;
+        cin >> a;
+        arr[a - 1] = i + 1;
     }
-    cout << group;
+    for(int x : arr){
+        cout << x << " ";
+    }
     return 0;
 }
