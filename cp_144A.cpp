1b257d5732cc373917c1c1fd7cde8e7f898c9019
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-08-21 22:33:27 +0530

    Refactor cp_144A.cpp to implement new logic for problem 469A and remove unused code

--- cp_144A.cpp
+++ cp_144A.cpp
@@ -1,25 +1,52 @@
+// #include <iostream>
+// #include <vector>
+// #include <climits>
+// using namespace std;
+// int main(){
+//     int n ; 
+//     cin >> n;
+//     vector<int> solder(n);
+//     int min_height = INT_MAX;
+//     int min_i = 0;
+//     int max_i = n - 1;
+//     int max_height = INT_MIN;
+//     for(int i = 0 ; i < n ; i++){
+//         cin >> solder[i];
+//         if(solder[i] <= min_height) min_height = solder[i], min_i = i;
+//         if(solder[i] >= max_height) max_height = solder[i], max_i = i;
+//     }
+//     if(min_i > max_i){
+//         cout << max_i + (n-1 - min_i);
+//     }
+//     else{
+//         cout << max_i + (n - 1 - min_i) - 1;
+//     }
+//     return 0;
+// }
+
+//=========================469A=====================
 #include <iostream>
-#include <vector>
-#include <climits>
+#include <set>
 using namespace std;
 int main(){
     int n ; 
     cin >> n;
-    vector<int> solder(n);
-    int min_height = INT_MAX;
-    int min_i = 0;
-    int max_i = n - 1;
-    int max_height = INT_MIN;
-    for(int i = 0 ; i < n ; i++){
-        cin >> solder[i];
-        if(solder[i] <= min_height) min_height = solder[i], min_i = i;
-        if(solder[i] >= max_height) max_height = solder[i], max_i = i;
+    set<int> level;
+    int p, q;
+    cin >> p;
+    for(int i = 0 ; i < p ; i++){
+        int a;
+        cin >> a;
+        level.insert(a);
     }
-    if(min_i > max_i){
-        cout << max_i + (n-1 - min_i);
+    cin >> q;
+    for(int i  = 0; i < q; i++){
+        int b;
+        cin >> b;
+        level.insert(b);
     }
-    else{
-        cout << max_i + (n - 1 - min_i) - 1;
-    }
+    int ss = level.size();
+    if(ss == n) cout << "I become the guy.";
+    else cout << "Oh, my keyboard!";
     return 0;
 }
