35a4133c6d6dad112dd5e5d93fb10a046367a6f2
Rudra-iitg <j.rudra@op.iitg.ac.in>
2026-09-09 01:40:38 +0530

    CPP

---
+++ 1335A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0; i < n; i++){
+        long long a;
+        cin >> a;
+        if(a % 2 == 0) cout << (a/2)-1;
+        else cout << a/2;
+    }
+    return 0;
+}
